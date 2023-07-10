1f290b6e3e59acc6ff8cc4cf4c54393eb2cc4238
Louis Maujean <lmaujean@c1r2p8.42nice.fr>
2023-07-10 14:51:43 +0200

    on peut commencer a bosser normalement

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'colyseus';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(3000);
+  const gameServer = new Server();
+  gameServer.define("myRoom", gameRoom);
 }
 bootstrap();
